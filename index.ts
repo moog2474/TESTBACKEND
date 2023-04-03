bbc96931e25e5ab53ffadd2b6863a8316f2314b7
Erdenedari <107394167+Daria61@users.noreply.github.com>
2023-04-04 00:08:42 +0800

    done

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Response, Request, Express } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import  restaurantRouter from "./routes/restaurants.route";
 
 const app : Express = express();
 const port = process.env.PORT;
@@ -8,7 +9,6 @@ const port = process.env.PORT;
 dotenv.config();
 const uri:string = process.env.PORT || ""
 
-const restaurantRouter = require("./routes/restaurants.route.js");
 
 mongoose
   .connect(uri)
@@ -19,7 +19,7 @@ mongoose
 app.use(express.json());
 app.use("/api", restaurantRouter);
 
-app.get("/api", (req, res) => {
+app.get("/api", (req :Request, res : Response) => {
   res.send("<h1>Hello MongoDB from NodeJS</h1>");
 });
 
